ffa917024e1009577a7012fdec3e4a2b85acbee3
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-11-07 08:40:42 +0600

    Update

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+        
+    return data
+
 
 @app.get("/")
 def hello():
-    return {'Message': "Hello World"}
+    return {'Message': "Patients Management System API"}
 
 
 @app.get('/about')
 def about():
-    return{'Message':'Shaon who always try to learn'}
+    return{'Message':'A fully functional API to manage your patient records.'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
